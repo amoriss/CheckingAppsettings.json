00d6a56072b218d1f84048791691ffc6b21f7477
Amoriss <amoriss@protonmail.com>
2024-05-06 11:58:19 -0500

    pushing up the rest of the files

---
+++ CheckingAppsettings.json/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace CheckingAppsettings.json.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
